532ee2bb88f759036371ca9302519f1927eacc92
“roselynn411” <roselynnfrempong@gmail.com>
2021-12-04 13:44:34 -0500

    first commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,33 @@
 // import files and packages up here
+const express = require('express');
+//install morgan to use it later
+const morgan = require('morgan');
+//install top spots data to use later
+const topSpots = require('./data');
 
 
 // create your express server below
-var app;
+const app = express();
 
-// add your routes and middleware below
+// // A port to launch the website 
+// const http = require('http');
+
+// add your routes and middleware below.  
+app.use(morgan('dev'));
+// const router = express.Router();
+// app.use('/',router);
+
+//✓ GET / respond
+app.get('/', function(req, res) {
+ console.log('I want to get the respond 200')
+ res.send("200")
+});
+
+//GET /data responds with all top spots
+app.get('/data', function(req, res){
+    console.log("Please send me the topspot data")
+    res.send(topSpots);
+});
 
 // finally export the express application
-module.exports = app;
+ module.exports = app;
